f0d2d57efd6179f550c23da8413602609d47fd54
Junior <nog.junior84@gmail.com>
2016-04-07 04:12:58 -0300

    Validates

--- app/models/courses.rb
+++ app/models/courses.rb
@@ -1,3 +1,7 @@
 class Courses < ActiveRecord::Base
+  validates_presence_of :name, message: "não pode ficar em branco"
+  validates_presence_of :description, message: "não pode ficar em branco"
+  validates_presence_of :status, message: "não pode ficar em branco"
+
   has_many :classroom
 end
--- app/models/students.rb
+++ app/models/students.rb
@@ -1,3 +1,7 @@
 class Students < ActiveRecord::Base
+  validates_presence_of :name, message: "não pode estar em branco"
+  validates_presence_of :register_number, message: "(RG) não pode ficar em branco"
+  validates_presence_of :status, message: "não pode ser indefinido"
+
   has_many :classroom
 end
